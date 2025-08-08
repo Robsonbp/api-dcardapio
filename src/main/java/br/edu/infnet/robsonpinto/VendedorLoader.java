b3d5e1541f6a390d35b96a95c81607906ee478df
Robson Pinto <robsonbpinto@outlook.com>
2025-08-07 22:29:34 -0300

    Acrescentado rotas complementares ao ProdutoController. Criado o arquivo produto.txt, com texto csv com informações de produtos. Modificado ProdutoLoader para puxar registro do arquivo produto.txt.

--- src/main/java/br/edu/infnet/robsonpinto/VendedorLoader.java
+++ src/main/java/br/edu/infnet/robsonpinto/VendedorLoader.java
@@ -1,5 +1,8 @@
 package br.edu.infnet.robsonpinto;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +15,26 @@ public class VendedorLoader implements ApplicationRunner {
 
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
-		Produto produto = new Produto();
 		
-		produto.nome = "Doce";
-		produto.descricao = "Doce muito gostoso";
-		produto.valor = 20.0;
-		produto.ativo = true;
+		FileReader arquivo = new FileReader("produto.txt");
+		BufferedReader leitura = new BufferedReader(arquivo);
 		
-		System.out.println(produto);
+		String[] campos = null;
 		
+		String linha = leitura.readLine();
+		while(linha != null) {
+			campos = linha.split(";");
+			
+			Produto produto = new Produto();
+			produto.nome = campos[0];
+			produto.descricao = campos[1];
+			produto.valor = Double.valueOf(campos[2]);
+			produto.ativo = Boolean.valueOf(campos[3]);
+			
+			System.out.println(produto);
+			linha = leitura.readLine();
+		}
+		
+		leitura.close();
 	}
 }
--- src/main/java/br/edu/infnet/robsonpinto/controller/ProdutoController.java
+++ src/main/java/br/edu/infnet/robsonpinto/controller/ProdutoController.java
@@ -1,12 +1,44 @@
 package br.edu.infnet.robsonpinto.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.edu.infnet.robsonpinto.model.domain.Produto;
+
 @RestController
+@RequestMapping("/api/produto")
 public class ProdutoController {
-	@GetMapping("/api/produto/nome")
+	
+	@GetMapping
+	public Produto pegarProduto() {
+		Produto produto = new Produto();
+		
+		produto.nome = "Doce";
+		produto.descricao = "Doce muito gostoso.";
+		produto.valor = 20.00;
+		produto.ativo = true;
+		
+		return produto;
+	}
+	
+	@GetMapping("/nome")
 	public String pegarNome() {
 		return "Doce";
+	}
+	
+	@GetMapping("/descricao")
+	public String pegarDescricao() {
+		return "Doce muito gostoso.";
+	}
+	
+	@GetMapping("/valor")
+	public double pegarValor() {
+		return 20.00;
+	}
+	
+	@GetMapping("/ativo")
+	public boolean pegarAtivo() {
+		return true;
 	}
 }
